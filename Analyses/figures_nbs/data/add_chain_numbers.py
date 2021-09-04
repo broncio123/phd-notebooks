87d74ecd5a138ff5d25926502150bd55570570a5
Broncio <ba13026@my.bristol.ac.uk>
2021-09-04 16:55:40 +0100

    added script to alter chain names in PDB

--- Analyses/figures_nbs/data/add_chain_numbers.py
+++ Analyses/figures_nbs/data/add_chain_numbers.py
@@ -1,13 +1,22 @@
 from pymol import cmd
 import sys
 
-in_pdb = sys.argv[1] # Input PDB path
-out_pdb = sys.argv[2] # Output PDB path
+pdb_in = sys.argv[1] # Input PDB path
+pdb_out = sys.argv[2] # Output PDB path
 
-cmd.load(in_pdb,"MyProtein")
-Chains = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+cmd.load(pdb_in,"MyProtein")
+N_atoms = cmd.count_atoms("MyProtein")
+
+Chains = ['A', 'B','C','D','E','F','G','H']
+
+atoms_per_chain = N_atoms/len(Chains)
+
 for k in range(len(Chains)):
-	cmd.alter("chain "+Chains[k],"resi=str(int(resi)-"+str(32*k)+")")
+    atom_number_intial = int( 1 + k*atoms_per_chain )
+    atom_number_final  = int( (k + 1)*atoms_per_chain )
+    selection = "id "+ str(atom_number_intial) + ":" + str(atom_number_final)
+    expression = "chain='"+Chains[k]+"'"
+    cmd.alter(selection, expression)
 
 cmd.set("retain_order",1)
-cmd.save(out_pdb ,"MyProtein")
+cmd.save(pdb_out ,"MyProtein")
